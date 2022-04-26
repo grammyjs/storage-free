e64a340392b9ad0a95d0ea94e6f3dd7d729e36f9
KnorpelSenf <shtrog@gmail.com>
2022-04-26 21:18:41 +0200

    Allow changing root URL

--- src/adapter.ts
+++ src/adapter.ts
@@ -1,11 +1,12 @@
-const rootUrl = "https://grammy-free-session.deno.dev";
-
-class Storage<T> {
+class Storage {
   private jwt: string | undefined;
-  constructor(private readonly token: string) {}
+  constructor(
+    private readonly token: string,
+    private readonly rootUrl = "http://localhost:8080",
+  ) {}
 
   async login() {
-    const url = `${rootUrl}/login`;
+    const url = `${this.rootUrl}/login`;
     const body = JSON.stringify({ token: this.token });
     const response = await fetch(url, { method: "POST", body });
     const { token } = await response.json();
@@ -21,7 +22,7 @@ class Storage<T> {
     body?: string,
   ): Promise<string | undefined> {
     // perform request
-    const url = `${rootUrl}/session/${key}`;
+    const url = `${this.rootUrl}/session/${key}`;
     if (this.jwt === undefined) await this.login();
     const headers = { "Authorization": `Bearer ${this.jwt}` };
     const response = await fetch(url, { method, body, headers });
@@ -43,10 +44,10 @@ class Storage<T> {
   }
 }
 
-export function freeStorage<T>(token: string) {
-  const storage = new Storage<T>(token);
+export function freeStorage<T>(token: string, opts?: { rootUrl?: string }) {
+  const storage = new Storage(token, opts?.rootUrl);
   return {
-    async read(key: string) {
+    async read(key: string): Promise<T> {
       const session = await storage.call("GET", key);
       return session === undefined ? undefined : JSON.parse(session);
     },
